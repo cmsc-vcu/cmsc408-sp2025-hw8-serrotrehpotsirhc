e598179f2c3fdcd6cefc649dbc93d64a45836afd
Christopher <Torresc6@vcu.edu>
2025-04-20 14:36:06 -0400

    Next

--- Countries Database.session.sql
+++ Countries Database.session.sql
@@ -1,4 +1,9 @@
-
-SELECT `Short Name`, `Table Name`, Region, `Income Group`
-FROM world_bank_data.wdi_country
-WHERE `Short Name` = 'Venezuela'
+SELECT
+  `Short Name`,
+  `Table Name`,
+  Region,
+  `Income Group`
+FROM
+  wdi_country
+WHERE
+  `Short Name` = 'Venezuela';
