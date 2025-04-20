0a7fb85a17e1c8038dc2d77392d95fc055961798
Christopher <Torresc6@vcu.edu>
2025-04-20 15:34:46 -0400

    Testing!

--- Countries Database.session.sql
+++ Countries Database.session.sql
@@ -1,1 +1,15 @@
-DESCRIBE wdi_country;
+SELECT
+  Region,
+  SUM(CASE WHEN `Income Group` = 'High income' THEN 1 ELSE 0 END) AS "High",
+  SUM(CASE WHEN `Income Group` = 'Upper middle income' THEN 1 ELSE 0 END) AS "Upper middle",
+  SUM(CASE WHEN `Income Group` = 'Lower middle income' THEN 1 ELSE 0 END) AS "Lower middle",
+  SUM(CASE WHEN `Income Group` = 'Low income' THEN 1 ELSE 0 END) AS "Low",
+  SUM(CASE WHEN `Income Group` IS NULL THEN 1 ELSE 0 END) AS "Other"
+FROM
+  world_bank_data.wdi_country
+WHERE
+  Region IS NOT NULL
+GROUP BY
+  Region
+ORDER BY
+  Region;
