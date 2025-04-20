e675ef135ebc8a4866b05143a9a43f806a7b2c8a
Christopher <Torresc6@vcu.edu>
2025-04-20 16:37:27 -0400

    Fix

--- Countries Database.session.sql
+++ Countries Database.session.sql
@@ -1,15 +1,14 @@
-SELECT
-  Region,
-  SUM(CASE WHEN `Income Group` = 'High income' THEN 1 ELSE 0 END) AS "High",
-  SUM(CASE WHEN `Income Group` = 'Upper middle income' THEN 1 ELSE 0 END) AS "Upper middle",
-  SUM(CASE WHEN `Income Group` = 'Lower middle income' THEN 1 ELSE 0 END) AS "Lower middle",
-  SUM(CASE WHEN `Income Group` = 'Low income' THEN 1 ELSE 0 END) AS "Low",
-  SUM(CASE WHEN `Income Group` IS NULL THEN 1 ELSE 0 END) AS "Other"
-FROM
-  world_bank_data.wdi_country
-WHERE
-  Region IS NOT NULL
-GROUP BY
-  Region
-ORDER BY
-  Region;
+-- Check before update
+SELECT COUNT(*)
+FROM wdi_country
+WHERE `Short Name` = 'Venezuela';
+
+-- Perform update
+UPDATE wdi_country
+SET Region = 'Latin America & Caribbean', `Income Group` = 'High income'
+WHERE `Short Name` = 'Venezuela';
+
+-- Check after update
+SELECT COUNT(*)
+FROM wdi_country
+WHERE `Short Name` = 'Venezuela';
