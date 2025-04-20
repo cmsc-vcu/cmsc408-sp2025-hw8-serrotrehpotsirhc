18a46156370d3258a6b06a5178645085ab3acf2f
Christopher <Torresc6@vcu.edu>
2025-04-20 16:48:35 -0400

    Complete!

--- Countries Database.session.sql
+++ Countries Database.session.sql
@@ -1,14 +1,1 @@
--- Check before update
-SELECT COUNT(*)
-FROM wdi_country
-WHERE `Short Name` = 'Venezuela';
-
--- Perform update
-UPDATE wdi_country
-SET Region = 'Latin America & Caribbean', `Income Group` = 'High income'
-WHERE `Short Name` = 'Venezuela';
-
--- Check after update
-SELECT COUNT(*)
-FROM wdi_country
-WHERE `Short Name` = 'Venezuela';
+DESCRIBE wdi_country;
